708d500b8722b3074f1a321eff4b205fd77343da
Robert Jeon <60145948+Error4042312@users.noreply.github.com>
2020-03-12 10:23:59 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,8 @@
-
+#include <stdio.h>
+//robert jeon hello.c
+int main (void) {
+    printf("hello, world\n");
+    printf("My name is Bob, my age is:");
+    int age = 16;
+    printf("%i.\n", age);
+}
